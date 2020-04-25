d3eaa0992f4e5db07462d1ccbca715f8a7bbb9c5
meghnavarma0 <meghnavarma0@gmail.com>
2020-04-25 15:20:08 +0530

    event bubbling and capturing

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const grandparent = document.getElementById('grandparent');
+const parent = document.getElementById('parent');
+const child = document.getElementById('child');
 
-let items = localStorage.getItem('items')
-	? JSON.parse(localStorage.getItem('items'))
-	: [];
-const data = JSON.parse(localStorage.getItem('items'));
+// According to W3C standatds, event capturing occurs followed by event bubbling.
+// Capturing : Trickling: Top - Down Propagation
+// Bubbling : Bottom - Up Propagation
 
-// localStorage.setItem('items', JSON.stringify(items));
-
-const liMaker = text => {
-	const li = document.createElement('li');
-	li.textContent = text;
-	ul.appendChild(li);
-};
-if (data) {
-	data.map(item => liMaker(item));
-}
-
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	items.push(input.value);
-	localStorage.setItem('items', JSON.stringify(items));
-	liMaker(input.value);
-	input.value = '';
-});
-
-button.addEventListener('click', () => {
-	localStorage.clear();
-	items = [];
-	while (ul.firstChild) ul.removeChild(ul.firstChild);
-});
+grandparent.addEventListener(
+	'click',
+	e => {
+		console.log('Grand Parent Clicked');
+		e.stopPropagation(); // Prevents further propagation of the event
+	},
+	true // Capturing
+);
+parent.addEventListener(
+	'click',
+	() => {
+		console.log('Parent Clicked');
+	},
+	false // Bubbling
+);
+child.addEventListener(
+	'click',
+	() => {
+		console.log('Child Clicked');
+	},
+	false // Bubbling
+);
