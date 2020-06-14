a9a2f26723c9bb284ee8753a10951a6a7acaace0
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-14 23:02:26 +0530

    0/1 knapsack problem added

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-// Program to find the number of 1s in binary representation of a number.
-
-const noOfOnes = num => {
-	let count = 0;
-	while (num) {
-		if (num & 1) {
-			count += 1;
+// Given a set of items, each with a weight and a value, determine the number of each item to include in a collection so that the total weight is less than or equal to a given limit(w) and the total value is as large as possible.
+function knapsack(val, weight, n, w) {
+	var mem = [];
+	for (let i = 0; i < n + 1; i++) {
+		let intermediate_array = [];
+		for (let j = 0; j < w + 1; j++) {
+			intermediate_array.push(0);
 		}
-		num = num >> 1;
+		mem.push(intermediate_array);
 	}
-	return count;
-};
-console.log(noOfOnes(2)); // 1
-console.log(noOfOnes(3)); // 2
-console.log(noOfOnes(7)); // 3
-console.log(noOfOnes(8)); // 1
 
-// 2 -> 10
-// 3 -> 11
-// 7 -> 111
-// 8 -> 1000
+	for (let i = 1; i < n + 1; i++) {
+		for (let j = 1; j < w + 1; j++) {
+			if (weight[i - 1] <= j) {
+				mem[i][j] = Math.max(
+					val[i - 1] + mem[i - 1][j - weight[i - 1]],
+					mem[i - 1][j]
+				);
+			} else {
+				mem[i][j] = mem[i - 1][j];
+			}
+		}
+	}
+
+	return mem[n][w];
+}
+val = [4, 6, 3, 9, 2];
+weight = [2, 12, 10, 5, 6];
+n = val.length;
+w = 15;
+console.log(knapsack(val, weight, n, w));
